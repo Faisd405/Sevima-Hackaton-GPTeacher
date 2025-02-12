3af613169853ce5a7a4634774ebd8412aaf84f38
faisd405 <faisd405@gmail.com>
2025-02-12 11:36:14 +0700

    refactir: Fix formatting in header title component

--- resources/views/components/header/title.blade.php
+++ resources/views/components/header/title.blade.php
@@ -11,8 +11,8 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
             </svg>
             <span class="text-sm font-semibold md:text-base text-slate-600 dark:text-slate-400">
-                {{ $backLinkName }} </span>
-
+                {{ $backLinkName }}
+            </span>
         </a>
     @endif
     <div class="mt-8">
